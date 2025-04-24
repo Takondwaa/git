642405697d0b7bc1dbbf3654035543c80df9442f
Takondwaa <bsc-com-ne-15-23@unima.ac.mw>
2025-04-24 19:28:53 +0200

    Update Person.cpp

--- Lab 8/Person.cpp
+++ Lab 8/Person.cpp
@@ -8,11 +8,24 @@ Person::Person()
     mAge = 0;
 }
 
-Person::Person(const string& name, float newWeight)
+Person::Person(const string& name, float newWeight, int age)
 {
     mWeight = newWeight;
-   mFirstName = "Unknown";
-    mAge = 0;
+   mFirstName = name;
+    mAge = age;
+}
+
+// Conversion operators
+Person::operator int() const {
+    return mAge;
+}
+
+Person::operator string() const {
+    return mFirstName;
+}
+
+Person::operator float() const {
+    return mWeight;
 }
 
 Person::~Person()
@@ -25,4 +38,24 @@ Person::~Person()
 float Person::operator+(const Person& otherPerson)
 {
     return this->mWeight + otherPerson.mWeight;
+}
+
+// Overload == to compare names
+bool Person::operator==(const Person& other) const {
+    return mFirstName == other.mFirstName;
+}
+
+// Overload != to compare names
+bool Person::operator!=(const Person& other) const {
+    return !(*this == other);
+}
+
+// Overload < to compare ages
+bool Person::operator<(const Person& other) const {
+    return mAge < other.mAge;
+}
+
+// Overload > to compare ages
+bool Person::operator>(const Person& other) const {
+    return mAge > other.mAge;
 }
