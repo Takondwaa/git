7e9d699411e01a8a4f823ba566d779235f7ab225
Takondwaa <bsc-com-ne-15-23@unima.ac.mw>
2025-04-24 19:29:59 +0200

    Update main2.cpp

--- Lab 8/main2.cpp
+++ Lab 8/main2.cpp
@@ -1,13 +1,40 @@
 #include <iostream>
 #include "Person.cpp"
+#include <string>
 
 int main(){
-    Person Jane = Person("Jane", 60.0f);
-    Person John = Person("John", 75.0f);
+    Person Jane = Person("Jane", 60.0f, 25);
+    Person John = Person("John", 75.0f, 30);
 
     float totalWeight = Jane + John;
 
     cout << "Total weight: " << totalWeight << endl;
+
+    if (Jane == John){
+        cout << "This is the same person " << endl;
+    }
+
+    if (Jane != John){
+        cout << "This is NOT the same person" << endl;
+    }
+
+    if (Jane < John) {
+        cout << "Jane is younger than John" << endl;
+    }
+    
+    if (John > Jane) {
+        cout << "John is older than Jane" << endl;
+    }
+
+    int johnAge = John;
+    cout << "John's Age: " << johnAge << endl;
+
+    string janeFirstName = Jane;
+    cout << "Jane's FirstName: " << janeFirstName << endl;
+    
+    float janeWeight = Jane;
+    cout << "Jane's weight: " << janeWeight << endl;
+
 
     return 0;
 }
