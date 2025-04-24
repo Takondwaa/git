fc7e80ddffe2ae3ef2609da5f0a98e1793eb6843
Takondwaa <bsc-com-ne-15-23@unima.ac.mw>
2025-04-24 19:27:48 +0200

    Update Person.h

--- Lab 8/Person.h
+++ Lab 8/Person.h
@@ -5,10 +5,19 @@ using namespace std;
 class Person{
     public:
     Person();
-    Person(const string& name, float newWeight);
+    Person(const string& name, float newWeight, int age);
     ~Person();
     //Overload the add operator
     float operator + (const Person& otherPerson);
+    bool operator==(const Person& other) const; // For mFirstName comparison
+    bool operator!=(const Person& other) const;
+    bool operator<(const Person& other) const;  // For mAge comparison
+    bool operator>(const Person& other) const;
+
+    // Conversion operators
+    operator int() const;
+    operator string() const;
+    operator float() const;
 
     private:
     float mWeight;
